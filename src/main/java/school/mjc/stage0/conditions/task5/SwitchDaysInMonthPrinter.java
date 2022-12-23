57a039e9f9d7e89a9652b5b718454e9408ee7254
Madara <ranel28021998@gmail.com>
2022-12-23 15:48:08 +0300

    It's done! AgaiN!

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -21,16 +21,9 @@ public class SwitchDaysInMonthPrinter {
             case 2:
                 System.out.println(28);
                 break;
-        }
-
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-            System.out.println("31");
-        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-            System.out.println("30");
-        } else if (month == 2) {
-            System.out.println(28);
-        } else {
-            System.out.println("wrong number!");
+            default:
+                System.out.println("wrong number!");
+                break;
         }
     }
 }
